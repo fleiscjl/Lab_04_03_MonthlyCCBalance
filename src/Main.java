b6b8725b284469d5e066f04df711bf754193e62e
fleiscjl <113860663+fleiscjl@users.noreply.github.com>
2022-09-20 15:03:05 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+
+    { System.out.println();
+
+        String Month;
+     int Balance = 5000;
+     double intRate = 0.17;
+     double Interest;
+     int T = 2;
+     Interest = Balance * intRate;
+
+     System.out.println("Interest due after one month is" + " " + Interest);
+     System.out.println("Interest due after two months is" + " " + Interest * T);
     }
 }
